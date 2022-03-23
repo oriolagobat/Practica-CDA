6c9ac0d50ea5a68078bf752dea2c4fbe22e5207b
Oriol Agost Batalla <oriolagobat@gmail.com>
2022-03-23 11:17:51 +0100

    Mini activitat 1: Dynamodb

--- practices/practice2/delete.ts
+++ practices/practice2/delete.ts
@@ -1,6 +1,6 @@
 import {DynamoDB} from "aws-sdk";
 
-const handler = async(event: any, _context: any) => {
+const handler = async(event: any, _context: any): Promise<void> => {
     const client = new DynamoDB.DocumentClient()
     await client.delete({
         TableName: 'oriol-agost-dynamodb-table',
--- practices/practice2/put.ts
+++ practices/practice2/put.ts
@@ -1,6 +1,6 @@
 import {DynamoDB} from "aws-sdk";
 
-const handler = async (event: any, _context: any) => {
+const handler = async (event: any, _context: any): Promise<void> => {
     const client = new DynamoDB.DocumentClient()
     await client.put({  // Callback, per l'await i el .promise() d'ultima línia fa que s'hagi de executar de una.
         Item: {
